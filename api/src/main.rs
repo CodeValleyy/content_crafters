aa7761dfb79ef017edf08f18462031586ea4c276
James <james.bidule@gmail.com>
2024-04-23 11:48:14 +0200

    chore: Add dotenv initialization in main function
    Initialize dotenv to load environment variables before logger setup.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,13 @@
 use api::web::run_server;
 use cli::parser::{CliApiArgs, Parser};
-use std::error::Error;
+use dotenv::dotenv;
 use logger::init_logger;
+use std::error::Error;
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let cli_args: CliApiArgs = CliApiArgs::parse();
+    dotenv().ok();
     init_logger(cli_args.verbose, cli_args.debug, cli_args.trace)?;
     run_server().await?;
     Ok(())
