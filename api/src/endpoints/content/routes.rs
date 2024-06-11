1cdcb8f55bea4397630e1dfbe65148a9f75f2095
James ABIB <james.bidule@gmail.com>
2024-06-11 21:50:15 +0200

    refacto:  Split content handlers into separate modules
    - Moved content handling functions into separate modules for better organization and readability.
    - Created new files for each module: 'metadata.rs', 'upload.rs', and 'version.rs'.
    - Updated routes to reflect the changes in function locations.

--- api/src/endpoints/content/routes.rs
+++ api/src/endpoints/content/routes.rs
@@ -1,30 +1,16 @@
 use actix_web::web;
 
-use super::handlers::{
-    add_comment, add_like, create_version, delete, delete_comment, get_details, get_version,
-    list_versions, remove_like, update_comment, update_metadata, upload,
-};
+use super::{metadata, upload, version};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/content")
-            .route("/upload", web::post().to(upload))
-            .route("/{id}", web::get().to(get_details))
-            .route("/{id}", web::put().to(update_metadata))
-            .route("/{id}", web::delete().to(delete))
-            .service(
-                web::scope("/{id}/comment")
-                    .route("", web::post().to(add_comment))
-                    .route("/{comment_id}", web::put().to(update_comment))
-                    .route("/{comment_id}", web::delete().to(delete_comment)),
-            )
-            .route("/{id}/like", web::post().to(add_like))
-            .route("/{id}/like", web::delete().to(remove_like))
-            .service(
-                web::scope("/{id}/versions")
-                    .route("", web::get().to(list_versions))
-                    .route("", web::post().to(create_version))
-                    .route("/{version}", web::get().to(get_version)),
-            ),
+            .route("/upload", web::post().to(upload::upload))
+            .route("/{id}", web::get().to(metadata::get_details))
+            .route("/{id}", web::put().to(metadata::update_metadata))
+            .route("/{id}", web::delete().to(metadata::delete))
+            .route("/versions", web::get().to(version::list_versions))
+            .route("/versions", web::post().to(version::create_version))
+            .route("/versions/{id}", web::get().to(version::get_version)),
     );
 }
