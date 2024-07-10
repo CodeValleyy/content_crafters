13fb2682cb95b319c01fa2d40297ff5148ebdaa2
James <james.bidule@gmail.com>
2024-07-10 12:09:46 +0200

    feat(upload): add message upload

--- shared/src/models/upload_file.rs
+++ shared/src/models/upload_file.rs
@@ -17,3 +17,15 @@ pub struct UploadGroupFile {
     #[schema(example = 123)]
     pub owner_id: i32,
 }
+
+#[derive(ToSchema)]
+pub struct UploadGroup {
+    #[schema(example = "vec![0, 1, 2, 3]")]
+    pub file: Vec<u8>,
+    #[schema(example = 123)]
+    pub group_id: i32,
+    #[schema(example = 123)]
+    pub owner_id: i32,
+    #[schema(example = 123)]
+    pub message_id: Option<i32>,
+}
